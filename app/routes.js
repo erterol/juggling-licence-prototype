e7bcc6cc39e7801cf3ec49c58299abe7a7f08cd8
Ert <eft@Bearclaw.local>
2019-10-11 15:02:28 +0100

    added the logic around cant juggle

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+/// Branching
+router.post('/juggling-trick', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let canJuggle = req.session.data['how-many-balls']
+
+  if (canJuggle === 'None - I cannot juggle') {
+    res.redirect('/cannot-juggle')
+  } else {
+    res.redirect('/juggling-trick')
+  }
+})
 
 module.exports = router
